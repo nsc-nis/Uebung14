82d326f2f6fdb5e93294852562bde84883d8be60
Niklas Schachl <niklas.schachl@htl-wels.at>
2021-02-18 10:26:19 +0100

    Built project structure

---
+++ src/at/nsc/controller/MainController.java
@@ -0,0 +1,4 @@
+package at.nsc.controller;
+
+public class MainController {
+}
---
+++ src/at/nsc/main/Main.java
@@ -0,0 +1,22 @@
+package at.nsc.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Niklas Schachl
+ * @version 1.0, 18.2.2021
+ */
+public class Main extends Application
+{
+    public static void main (String[] args)
+    {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+
+    }
+}
